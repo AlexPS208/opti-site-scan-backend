bf24a20a7c4fd4a98131a7802d663270941731b3
Alexander <magnum74338@gmail.com>
2023-12-02 18:53:08 +0200

    Add mail sender

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { ApiResponse, UseGoogleAPI } from './src/controllers/googleAPI'
+import multer, { Multer, StorageEngine } from 'multer'
+import nodemailer, { Transporter } from 'nodemailer'
 
 dotenv.config()
 
@@ -23,6 +25,19 @@ app.use(cors({
   methods: ['GET', 'POST']
 }))
 
+// Setting storage for multer
+const storage: StorageEngine = multer.memoryStorage()
+const upload: Multer = multer({ storage: storage })
+
+// Настройка Nodemailer
+const transporter: Transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAILNAME,
+    pass: process.env.GMAILKEY
+  },
+})
+
 
 app.get('/', (req: Request, res: Response) => {
   res.status(204).send()
@@ -33,6 +48,43 @@ app.post('/', (req: Request, res: Response) => {
   response.then( response => 
     res.status(response.status).send(response.response)
   )
+})
+
+app.post('/sendpdf/', upload.single('file'), (req: Request, res: Response) => {
+  const pdfBuffer: Buffer = req.file!.buffer
+
+  const name: string | undefined = req.body.name
+  const gmail: string | undefined = req.body.gmail
+
+  if(!gmail) {
+    res.status(500).send('No gmail in request')
+  }
+
+  // Send dudes
+  const mailOptions: nodemailer.SendMailOptions = {
+    from: process.env.GMAILNAME,
+    to: gmail, 
+    subject: 'Site Speed Analitics',
+    html: `<span style="font-size:18px; color: #000">
+      Hello, ${name? name : 'user'}, there's your speed analitics.
+    </span><br>
+    <span style="color: #000">
+      Message generated automatically. Please do not reply to this letter.
+    </span>`,
+    attachments: [
+      {
+        filename: 'Speed Analytics.pdf',
+        content: pdfBuffer
+      },
+    ],
+  }
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      return res.status(500).send(error.toString())
+    }
+    res.status(204).send()
+  })
 })
 
 
